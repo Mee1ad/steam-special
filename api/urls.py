1a0cf5c9ffeff9fbafa4540ae90a920410e91732
Meelad <soheilravasani@gmail.com>
2023-05-07 23:00:41 +0300

    fix urls

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('games/', views.steam_games, name='game_list'),
-    path('games2/', views.scrape_games, name='game_list2'),
+    path('games', views.scrape_games, name='game_list'),
 ]
